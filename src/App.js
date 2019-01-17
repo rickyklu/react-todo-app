c35b2d12b3c3e8c5f2943e2c4f76d1ec7efc1550
Ricky Lu <real0st@gmail.com>
2019-01-17 21:30:12 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+	          <a className="navbar-brand" href="#navbar" target="_blank">
+	          	<img src={logo} width="30" height="30" alt="Navbar"/>
+	          </a>
+	          <Link to="/" className="navbar-brand">Todo App</Link>
+	          <div className="collapse navbar-collapse">
+	          	<ul className="navbar-nav mr-auto">
+	          		<li className="navbar-item">
+	          			<Link to="/" className="nav-link">Todos</Link>
+	          		</li>
+	          		<li className="navbar-item">
+	          			<Link to="/create" className="nav-link">Create Todo</Link>
+	          		</li>
+	          	</ul>
+	          </div>
+          </nav>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" exact component={EditTodo} />
+          <Route path="/create" exact component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+export default class CreateTodo extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			todo_description: '',
+			todo_responsible: '',
+			todo_priority: '',
+			todo_completed: false
+		}
+		this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+		this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+		this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onChangeTodoDescription(e){
+		this.setState({
+			todo_description: e.target.value
+		});
+	}
+
+	onChangeTodoResponsible(e){
+		this.setState({
+			todo_responsible: e.target.value
+		});
+	}
+
+	onChangeTodoPriority(e){
+		this.setState({
+			todo_priority: e.target.value
+		});
+	}
+
+	onSubmit(e){
+		e.preventDefault();
+		console.log("Form submitted");
+
+		const newTodo = {
+			todo_description: this.state.todo_description,
+			todo_responsible: this.state.todo_responsible,
+			todo_priority: this.state.todo_priority,
+			todo_completed: this.state.todo_completed
+		}
+
+		axios.post("http://localhost:4000/todos/add", newTodo)
+			.then( res => console.log(res.data));
+
+		// reset todo	
+		this.setState({
+			todo_description: '',
+			todo_responsible: '',
+			todo_priority: '',
+			todo_completed: false
+		})
+	}
+
+	render(){
+		return (
+			<div style={{ marginTop: 10 }}>
+				<h3> Create New Todo </h3>
+				<form onSubmit={ this.onSubmit }>
+					<div className="form-group">
+						<label>Description: </label>
+						<input 
+							type="text"
+							className="form-control"
+							value={ this.state.todo_description }
+							onChange={ this.onChangeTodoDescription }
+						/>
+					</div>
+					<div className="form-group">
+						<label>Responsible: </label>
+						<input 
+							type="text"
+							className="form-control"
+							value={ this.state.todo_responsible }
+							onChange={ this.onChangeTodoResponsible }
+						/>
+					</div>
+					<form className="form-inline mb-3">
+						<div className="form-group">
+							<div className="form-check form-check-inline">
+								<input 
+									type="radio"
+									name="priorityOptions"
+									className="form-check-input"
+									value="Low"
+									checked={ this.state.todo_priority === 'Low' }
+									onChange={ this.onChangeTodoPriority }
+								/>
+								<label className="form-check-label">Low</label>
+							</div>
+						</div>
+
+						<div className="form-group">
+							<div className="form-check form-check-inline">
+								<input 
+									type="radio"
+									name="priorityOptions"
+									className="form-check-input"
+									value="Medium"
+									checked={ this.state.todo_priority === 'Medium' }
+									onChange={ this.onChangeTodoPriority }
+								/>
+								<label className="form-check-label">Medium</label>
+							</div>
+						</div>
+
+						<div className="form-group">
+							<div className="form-check form-check-inline">
+								<input 
+									type="radio"
+									name="priorityOptions"
+									className="form-check-input"
+									value="High"
+									checked={ this.state.todo_priority === 'High' }
+									onChange={ this.onChangeTodoPriority }
+								/>
+								<label className="form-check-label">High</label>
+							</div>
+						</div>
+					</form>
+					
+
+					<div className="form-group">
+						<input
+							type="submit" value="Create Todo" className="btn btn-primary"
+						/>
+					</div>
+
+				</form>
+			</div>
+		)
+	}
+
+}
